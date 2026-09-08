218189b06121c3eb6b9f7a173c97c9452e4541e3
11646729 <bds6052@gmail.com>
2026-09-08 21:08:23 +0100

    Initial Commit

---
+++ swift-putt-tracker/swift_putt_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_putt_trackerApp.swift
+//  swift-putt-tracker
+//
+//  Created by BRIAN SMITH on 08/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct swift_putt_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
